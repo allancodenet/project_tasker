e049a2736d77cfe1b7baba1a3e741d342d853efd
allancodenet <allancodenet@gmail.com>
2024-06-21 05:11:29 +0300

    auto update completed_at

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ApplicationRecord
   belongs_to :project
   enum :priority, {high: 0, medium: 1, low: 2}
+  after_update :update_completed_at
+
+  def update_completed_at
+    if completed?
+      update_column(:completed_at, Time.current)
+    else
+      update_column(:completed_at, nil)
+    end
+  end
 end
