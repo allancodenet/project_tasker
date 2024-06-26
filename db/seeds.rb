bf4de2d0b0009164a9195e475524f76e91fe727c
allancodenet <allancodenet@gmail.com>
2024-06-26 03:55:54 +0300

    added seeding data using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create!(
+  email: "test@test.com",
+  password: "password"
+)
+projects = 20.times.map do
+  Project.create!(name: Faker::Educator.unique.subject, user: user)
+end
+projects.each do |project|
+  20.times do
+    Task.create!(name: "read #{Faker::ProgrammingLanguage.unique.name}",
+      project: project, priority: rand(0..2), due_date: Faker::Date.forward(days: 23))
+  end
+end
