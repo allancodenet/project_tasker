ed6962077c1aa4faa8d5353276d88f70ecd65d3f
allancodenet <allancodenet@gmail.com>
2024-07-19 11:38:10 +0300

    real time updates

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, uniqueness: {case_sensitive: false}
+  broadcasts_refreshes
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  belongs_to :project
+  belongs_to :project, touch: true
   has_many :noticed_events, as: :record, dependent: :destroy, class_name: "Noticed::Event"
   has_many :notifications, through: :noticed_events, class_name: "Noticed::Notification"
   validates :name, :due_date, presence: true
