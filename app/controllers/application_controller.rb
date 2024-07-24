3dd75ba434e1252227c5641dda864f1680cb8f50
allancodenet <allancodenet@gmail.com>
2024-07-24 17:41:22 +0300

    accepted nested attributes for organization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pagy::Backend
+
+  protected
+
   def after_sign_in_path_for(current_user)
     dashboard_index_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, owned_organization_attributes: [:name, :subdomain]])
   end
 end
