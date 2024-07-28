a1b0ca9b9aa919503f07c706842365651622af84
allancodenet <allancodenet@gmail.com>
2024-07-28 18:38:57 +0300

    added tenant to email delivery

--- app/jobs/urgent_task_job.rb
+++ app/jobs/urgent_task_job.rb
@@ -4,8 +4,10 @@ class UrgentTaskJob < ApplicationJob
   def perform(*args)
     ActsAsTenant.without_tenant do
       urgent_tasks = Task.urgent
-      urgent_tasks.each do |task|
-        UrgentTaskNotifier.with(record: task).deliver(task.assignee)
+      urgent_tasks.find_each do |task|
+        ActsAsTenant.with_tenant(task.organization) do
+          UrgentTaskNotifier.with(record: task).deliver(task.assignee)
+        end
       end
     end
   end
